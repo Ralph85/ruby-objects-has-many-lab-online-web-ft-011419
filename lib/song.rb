9172d8c7757f4017d6e487b445be74589f1e1037
Ralph85 <Ralph.saintalbord@yahoo.com>
2019-09-20 10:55:32 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,36 @@
 class Song
-  attr_accessor :title, :artist
+  
+  attr_accessor :name, :artist
 
   @@all = []
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+    @artist = artist
+
+    @@all << self
+  end
+
+  def artist=(artist)
+    @artist = artist
+    @artist.songs << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+
+end #End Of Class
